651ba6d528dfd04246d385649731948ee83bb148
Kazi Islam <kazichaska@gmail.com>
2022-06-18 12:58:45 -0500

    login route and page works

--- app/models/User.py
+++ app/models/User.py
@@ -12,6 +12,12 @@ class User(Base):
     username = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
+    
+    def verify_password(self, password):
+        return bcrypt.checkpw(
+            password.encode('utf-8'),
+            self.password.encode('utf-8')
+        )
 
     @validates('email')
     def validate_email(self, key, email):
@@ -25,3 +31,5 @@ class User(Base):
 
         # encrypt password
         return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,15 +1,37 @@
-from flask import Blueprint, render_template
+from webbrowser import get
+from flask import Blueprint, render_template, session, redirect
+from app import db
+from app.models import Post
+from app.db import get_db
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    return render_template('homepage.html')
+    # get all posts
+    db = get_db()
+    posts = db.query(Post).order_by(Post.created_at.desc()).all()
+    return render_template(
+        'homepage.html',
+        posts=posts,
+        loggedIn = session.get('loggedIn')
+    )
 
 @bp.route('/login')
 def login():
-    return render_template('login.html')
+    # not logged in yet
+    if session.get('loggedIn') is None:
+        return render_template('login.html')
+    return redirect('/dashboard')
 
 @bp.route('/post/<id>')
 def single(id):
-    return render_template('single-post.html')
+    # get sindle post by id
+    db = get_db()
+    post = db.query(Post).filter(Post.id == id).one()
+    # render single post template
+    return render_template(
+        'single-post.html', 
+        post=post,
+        loggedIn=session.get('loggedIn')
+    )
